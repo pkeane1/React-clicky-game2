55839555162e0ff52b74eb17bf13dc2bbf1db437
Pierce Keane <Pierce.Keane@gmail.com>
2019-09-05 22:18:02 -0400

    Updates

---
+++ precache-manifest.f558c542507dc75675b55fdb0180b7c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f3f00769161132995f36d234539389",
+    "url": "/React-clicky-game2/index.html"
+  },
+  {
+    "revision": "1e79dbe2f727e677817f",
+    "url": "/React-clicky-game2/static/css/main.a5043902.chunk.css"
+  },
+  {
+    "revision": "6288d8aa4f595bae2894",
+    "url": "/React-clicky-game2/static/js/2.b99e9fd8.chunk.js"
+  },
+  {
+    "revision": "1e79dbe2f727e677817f",
+    "url": "/React-clicky-game2/static/js/main.fd3037fa.chunk.js"
+  },
+  {
+    "revision": "e2d0a4e43f6e0b23d78c",
+    "url": "/React-clicky-game2/static/js/runtime~main.5f00f8d7.js"
+  }
+]);

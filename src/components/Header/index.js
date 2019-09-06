30ead4e74b57c4a933947b5ca8774df5401c1839
Pierce Keane <Pierce.Keane@gmail.com>
2019-09-05 21:11:57 -0400

    path way on version one was incorrect. new repo

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css"
+
+const Header = (props) => {
+     return (
+     <div className = "header">
+        <div className = "title">{props.children}</div>
+        <div className="scores">
+          Score: {props.score} Highscore: {props.highscore}
+        </div>
+    </div>
+
+     )
+}
+
+export default Header;
+
+
